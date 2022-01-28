3f3d9432271ce938e56ae30d996adfae26d0727a
Daniel Cliche <dancliche@gmail.com>
2022-01-28 10:40:17 -0500

    Add auto reset

--- rtl/icebreaker/top.sv
+++ rtl/icebreaker/top.sv
@@ -4,9 +4,19 @@ module top(
     output logic P1B1, P1B2, P1B3, P1B4, P1B7, P1B8, P1B9, P1B10
     );
 
+    // reset
+    logic auto_reset;
+    logic [5:0] auto_reset_counter = 0;
     logic reset;
-    assign reset = !BTN_N;
 
+    assign auto_reset = auto_reset_counter < 5'b11111;
+    assign reset = auto_reset || !BTN_N;
+
+	always @(posedge CLK) begin
+		auto_reset_counter <= auto_reset_counter + auto_reset;
+	end
+
+    // display
     logic [7:0] display;
 
     always_comb begin
