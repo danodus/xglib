21b6bd98bc72318fede74d04085dad81859a61c3
Daniel Cliche <dancliche@gmail.com>
2022-05-30 18:09:14 -0400

    Fix execution issues with SDRAM

--- rtl/processor.sv
+++ rtl/processor.sv
@@ -60,14 +60,15 @@ module processor(
         DECODE2,
         DECODE3,
         WAIT_ACK,
-        WRITE
+        WRITE,
+        WRITE2
     } state;
 
     register_file register_file(
         .clk(clk),
         .in_i(reg_in),
         .in_sel_i(reg_in_sel),
-        .in_en_i(reg_in_en && state == WAIT_ACK),
+        .in_en_i(reg_in_en && state == WRITE),
         .out1_sel_i(reg_out1_sel),
         .out2_sel_i(reg_out2_sel),
         .out1_o(reg_out1),
@@ -84,7 +85,7 @@ module processor(
     );
 
     decoder decoder(
-        .en_i(state == DECODE || state == DECODE2 || state == DECODE3 || state == WAIT_ACK),
+        .en_i(state == DECODE || state == DECODE2 || state == DECODE3 || state == WAIT_ACK || state == WRITE),
         .instr_i(instruction),
         .reg_out1_i(reg_out1),
         .reg_out2_i(reg_out2),
@@ -162,7 +163,7 @@ module processor(
     end
 
     always_comb begin
-        d_addr = (state == DECODE || state == DECODE2 || state == DECODE3 || state == WAIT_ACK) ? addr : pc;
+        d_addr = (state == DECODE || state == DECODE2 || state == DECODE3 || state == WAIT_ACK || state == WRITE) ? addr : pc;
         reg_in = reg_in_source == 2'b01 ? d_data_out_ext : reg_in_source == 2'b10 ? pc + 4 : alu_out;
         alu_in1 = alu_in1_sel ? pc : reg_out1;
         alu_in2 = alu_in2_sel ? imm : reg_out2;
@@ -205,11 +206,14 @@ module processor(
                     //$display("reg_in_source: %d, addr: %h, we: %d, data_in: %x, data_out: %x", reg_in_source, addr_o, we_o, data_in_i, data_out_o);
                     sel_o <= 1'b0;
                     we_o  <= 1'b0;
-                    pc <= next_pc;
                     state <= WRITE;
                 end
             end
             WRITE: begin
+                state <= WRITE2;
+                pc <= next_pc;
+            end
+            WRITE2: begin
                 state <= FETCH;
             end
         endcase
