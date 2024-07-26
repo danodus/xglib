9f1ea47713b055c31a399da5e03f8ac4d762a69d
Daniel Cliche <danclichegmail.com>
2024-07-26 14:14:34 -0400

    New UART based on Oberon project

--- rtl/uart.sv
+++ rtl/uart.sv
@@ -37,98 +37,35 @@ module uart #(
     output      logic       busy_o,
     output      logic       valid_o
 );
+    logic rx_rdy, tx_rdy;
+    assign busy_o = !tx_rdy;
+    assign valid_o = rx_rdy;
 
-    parameter divider = FREQ_HZ / BAUDS;
-   
-    logic [3:0] recv_state;
-    logic [31:0] recv_divcnt;   // Counts to divider. Reserve enough bytes !
-    logic [7:0] recv_pattern;
-    logic [7:0] recv_buf_data;
-    logic recv_buf_valid;
+    uart_rx #(
+        .FREQ_HZ(FREQ_HZ),
+        .BAUD_RATE(BAUDS)
+    ) uart_rx(
+        .clk(clk),
+        .rst(!reset_i),
+        .RxD(rx_i),
+        .fsel(1'b0),
+        .done(rd_i),
+        .rdy(rx_rdy),
+        .data(rx_data_o)
+    );
 
-    logic [9:0] send_pattern;
-    logic send_dummy;
-    logic [3:0] send_bitcnt;
-    logic [31:0] send_divcnt;   // Counts to divider. Reserve enough bytes !
-
-    always_comb begin
-        rx_data_o = recv_buf_data;
-        valid_o   = recv_buf_valid;
-        busy_o    = (send_bitcnt || send_dummy);
-    end
-
-    always_ff @(posedge clk) begin
-        if (reset_i) begin
-
-            recv_state     <= 0;
-            recv_divcnt    <= 0;
-            recv_pattern   <= 0;
-            recv_buf_data  <= 0;
-            recv_buf_valid <= 0;
-
-        end else begin
-            recv_divcnt <= recv_divcnt + 1;
-
-            if (rd_i) recv_buf_valid <= 0;
-
-            case (recv_state)
-                0: begin
-                    if (!rx_i)
-                        recv_state <= 1;
-                end
-                1: begin
-                    if (recv_divcnt > divider/2) begin
-                        recv_state  <= 2;
-                        recv_divcnt <= 0;
-                    end
-                end
-                10: begin
-                    if (recv_divcnt > divider) begin
-                        recv_buf_data  <= recv_pattern;
-                        recv_buf_valid <= 1;
-                        recv_state     <= 0;
-                    end
-                end
-                default: begin
-                   if (recv_divcnt > divider) begin
-                        recv_pattern <= {rx_i, recv_pattern[7:1]};
-                        recv_state   <= recv_state + 1;
-                        recv_divcnt  <= 0;
-                    end
-                end
-            endcase
-        end
-    end
-
-    assign tx_o = send_pattern[0];
-
-    always_ff @(posedge clk) begin
-        send_divcnt <= send_divcnt + 1;
-        if (reset_i) begin
-            send_pattern <= ~0;
-            send_bitcnt  <= 0;
-            send_divcnt  <= 0;
-            send_dummy   <= 1;
-        end else begin
-            if (send_dummy && !send_bitcnt) begin
-                send_pattern <= ~0;
-                send_bitcnt  <= 15;
-                send_divcnt  <= 0;
-                send_dummy   <= 0;
-            end else if (wr_i && !send_bitcnt) begin
-`ifndef SYNTHESIS
-                $display("UART TX: %x (%c)", tx_data_i, tx_data_i);
-`endif
-                send_pattern <= {1'b1, tx_data_i[7:0], 1'b0};
-                send_bitcnt  <= 10;
-                send_divcnt  <= 0;
-            end else if (send_divcnt > divider && send_bitcnt) begin
-                send_pattern <= {1'b1, send_pattern[9:1]};
-                send_bitcnt  <= send_bitcnt - 1;
-                send_divcnt  <= 0;
-            end
-        end 
-    end
+    uart_tx #(
+        .FREQ_HZ(FREQ_HZ),
+        .BAUD_RATE(BAUDS)
+    ) uart_tx(
+        .clk(clk),
+        .rst(!reset_i),
+        .start(wr_i),
+        .fsel(1'b0),
+        .data(tx_data_i),
+        .rdy(tx_rdy),
+        .TxD(tx_o)
+    );
 
 endmodule
 
